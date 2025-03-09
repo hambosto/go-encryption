79707dc26bcb45857a67c42e01128dbf1c0a980c
Ilham Putra Husada <hambosto@gmail.com>
2025-03-10 02:19:54 +0700

    chore: add flake files for nix derivation

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Secure file encryption and decryption CLI tool built with Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      pname = "go-encryption";
+      version = "1.0";
+
+      nixosModule =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.programs.${pname};
+        in
+        {
+          options.programs.${pname} = {
+            enable = lib.mkEnableOption "Enable the ${pname} application";
+          };
+
+          config = lib.mkIf cfg.enable {
+            home.packages = [ self.packages.${pkgs.system}.default ];
+          };
+        };
+
+      perSystem =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          package = pkgs.buildGoModule {
+            inherit pname version;
+            src = ./.;
+            vendorSha256 = "sha256-nxRo/spwxVE+B41znEJEWuHozxJ6dc/BAAFRU5TIYuk=";
+
+            env.CGO_ENABLED = 0;
+
+            ldflags = [
+              "-extldflags '-static'"
+              "-s -w"
+            ];
+          };
+        in
+        {
+          packages = {
+            default = package;
+            ${pname} = package;
+          };
+
+          apps.default = flake-utils.lib.mkApp {
+            drv = package;
+            name = pname;
+          };
+        };
+    in
+    flake-utils.lib.eachDefaultSystem perSystem
+    // {
+      nixosModules = {
+        default = nixosModule;
+        ${pname} = nixosModule;
+      };
+    };
+}
