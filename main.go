660a07243a5b1c6a6c178b6b4ec62c0189fe1229
Ilham Putra Husada <hambosto@gmail.com>
2025-04-14 19:19:21 +0700

    fix: handle command errors gracefully

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/hambosto/go-encryption/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/hambosto/go-encryption/cmd"
+)
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
